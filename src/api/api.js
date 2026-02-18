dfdccf2888383c0ce166813c39b1ff702bb8d4d5
Pranjal Supekar <pranjalsupekar91@gmail.com>
2026-02-18 15:15:15 +0530

    add render api

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://medcodebackend.onrender.com";
 
 export const getUsers = async () => {
   const res = await fetch(`${BASE_URL}/api/users`);
